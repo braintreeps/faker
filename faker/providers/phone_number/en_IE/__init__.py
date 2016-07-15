e67722d50a10dcdd143a46c5a095c59f790ab788
chrissie <code@getbraintree.com>
2016-07-15 20:21:06 +0000

    BTMKPL-750 - Add formats for en_IE

---
+++ faker/providers/phone_number/en_IE/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+from .. import Provider as PhoneNumberProvider
+
+
+class Provider(PhoneNumberProvider):
+    formats = (
+        '44 01 69 8# 1####',
+    )
