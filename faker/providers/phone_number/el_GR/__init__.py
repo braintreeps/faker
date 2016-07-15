d75c7e792a5fa7e2e81948485e2c7030f7cfe025
chrissie <code@getbraintree.com>
2016-07-15 21:02:20 +0000

    BTMKPL-750 - Amend formats for el_GR

--- faker/providers/phone_number/el_GR/__init__.py
+++ faker/providers/phone_number/el_GR/__init__.py
@@ -4,10 +4,5 @@ from .. import Provider as PhoneNumberProvider
 
 class Provider(PhoneNumberProvider):
     formats = (
-        '(+30) 69## ######',
-        '+30 69## ######',
         '+3069########',
-        '(+30) 2### ######',
-        '+30 2### ######',
-        '+302#########',
     )
