bb51625b4bc666085107c1e38252186e898c1dc6
chrissie <code@getbraintree.com>
2016-07-15 20:25:31 +0000

    BTMKPL-750 - Amend formats for es_ES

--- faker/providers/phone_number/es_ES/__init__.py
+++ faker/providers/phone_number/es_ES/__init__.py
@@ -4,10 +4,5 @@ from .. import Provider as PhoneNumberProvider
 
 class Provider(PhoneNumberProvider):
     formats = (
-        '+34 ### ### ###',
-        '+34 #########',
-        '+34 ### ## ## ##',
-        '+34### ### ###',
-        '+34#########',
-        '+34### ## ## ##',
+        '+34 900 ### ###',
     )
